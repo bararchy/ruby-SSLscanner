f0076f6130bdf1beb4e25990339cb86c10737e73
bararchy <bar.hofesh@safe-t.com>
2014-11-12 14:02:18 +0200

    Added support for 1.8 and 1.9

--- ssl_scanner.rb
+++ ssl_scanner.rb
@@ -6,22 +6,26 @@ require 'socket'
 
 class Scanner
 
+
 	def self.ssl_scan
-		trap("INT") do
-	  		puts "Exiting..."
-	  		break
-	  		exit 1
+	
+		# Check version for compatibility 
+		version = RUBY_VERSION 
+		if version.match(/1.9/) || version.match(/1.8/)
+			puts "Starting in compatibility mode\r\n"
+			scan_loop("test_all")
+		else
+			puts "\r\nTesting SSLv2"
+			scan_loop(OpenSSL::SSL::OP_NO_SSLv3 + OpenSSL::SSL::OP_NO_TLSv1 + OpenSSL::SSL::OP_NO_TLSv1_1 + OpenSSL::SSL::OP_NO_TLSv1_2)
+			puts "\r\nTesting SSLv3"
+			scan_loop(OpenSSL::SSL::OP_NO_SSLv2 + OpenSSL::SSL::OP_NO_TLSv1 + OpenSSL::SSL::OP_NO_TLSv1_1 + OpenSSL::SSL::OP_NO_TLSv1_2)
+			puts "\r\nTesting TLSv1"
+			scan_loop(OpenSSL::SSL::OP_NO_SSLv2 + OpenSSL::SSL::OP_NO_SSLv3 + OpenSSL::SSL::OP_NO_TLSv1_1 + OpenSSL::SSL::OP_NO_TLSv1_2)
+			puts "\r\nTesting TLSv1.1"
+			scan_loop(OpenSSL::SSL::OP_NO_SSLv2 + OpenSSL::SSL::OP_NO_SSLv3 + OpenSSL::SSL::OP_NO_TLSv1 + OpenSSL::SSL::OP_NO_TLSv1_2)
+			puts "\r\nTesting TLSv1.2"
+			scan_loop(OpenSSL::SSL::OP_NO_SSLv2 + OpenSSL::SSL::OP_NO_SSLv3 + OpenSSL::SSL::OP_NO_TLSv1 + OpenSSL::SSL::OP_NO_TLSv1_1)
 		end
-		puts "\r\nTesting SSLv2"
-		scan_loop(OpenSSL::SSL::OP_NO_SSLv3 + OpenSSL::SSL::OP_NO_TLSv1 + OpenSSL::SSL::OP_NO_TLSv1_1 + OpenSSL::SSL::OP_NO_TLSv1_2)
-		puts "\r\nTesting SSLv3"
-		scan_loop(OpenSSL::SSL::OP_NO_SSLv2 + OpenSSL::SSL::OP_NO_TLSv1 + OpenSSL::SSL::OP_NO_TLSv1_1 + OpenSSL::SSL::OP_NO_TLSv1_2)
-		puts "\r\nTesting TLSv1"
-		scan_loop(OpenSSL::SSL::OP_NO_SSLv2 + OpenSSL::SSL::OP_NO_SSLv3 + OpenSSL::SSL::OP_NO_TLSv1_1 + OpenSSL::SSL::OP_NO_TLSv1_2)
-		puts "\r\nTesting TLSv1.1"
-		scan_loop(OpenSSL::SSL::OP_NO_SSLv2 + OpenSSL::SSL::OP_NO_SSLv3 + OpenSSL::SSL::OP_NO_TLSv1 + OpenSSL::SSL::OP_NO_TLSv1_2)
-		puts "\r\nTesting TLSv1.2"
-		scan_loop(OpenSSL::SSL::OP_NO_SSLv2 + OpenSSL::SSL::OP_NO_SSLv3 + OpenSSL::SSL::OP_NO_TLSv1 + OpenSSL::SSL::OP_NO_TLSv1_1)
 	end
 
 
@@ -32,18 +36,28 @@ class Scanner
 	  		exit 1
 		end
 		ssl_context = OpenSSL::SSL::SSLContext.new
-		ssl_context.options = options
+		ssl_context.ciphers = "ALL::HIGH::MEDIUM::LOW::SSL23"
+		if options != "test_all"
+			ssl_context.options = options
+    	end
     	for cipher in ssl_context.ciphers
     		begin
     			sleep 0.1
     			ssl_context = OpenSSL::SSL::SSLContext.new
-    			ssl_context.options = options
+    			ssl_context.ciphers = "ALL::HIGH::MEDIUM::LOW::SSL23"
+    			if options != "test_all"
+					ssl_context.options = options
+    			end
 				ssl_context.verify_mode = OpenSSL::SSL::VERIFY_NONE	
 				tcp_socket = TCPSocket.new("#{ARGV[0]}", ARGV[1].to_i)
     			ssl_context.ciphers = cipher[0].to_s
     			socket_destination = OpenSSL::SSL::SSLSocket.new tcp_socket, ssl_context
     			socket_destination.connect
-    			puts "\e[0;32mServer supports #{cipher[0]} #{cipher[2]}\033[0m"
+    			if options != "test_all"
+    				puts "\e[0;32mServer Supports: #{cipher[0]} #{cipher[2]}\033[0m"
+    			else
+    				puts "\e[0;32mServer Supports: #{cipher[0]} #{cipher[1]} #{cipher[2]}\033[0m"
+    			end
     			socket_destination.close
     		rescue Exception => e
     			if e.to_s.match(/unsupported protocol/)
@@ -51,11 +65,19 @@ class Scanner
     				break
     			end
     			if ARGV[2] == "--no-error"
-    			else
-    				puts "\033[1;31mNo support for #{cipher[0]} #{cipher[2]}\033[0m #{e}"
+    			else	
+	    			if options != "test_all"
+	    				puts "\e[0;32mServer Supports: #{cipher[0]} #{cipher[2]}\033[0m"
+	    			else
+	    				puts "\e[0;32mServer Supports: #{cipher[0]} #{cipher[1]} #{cipher[2]}\033[0m"
+	    			end
     			end
     		end
     	end
+    end
+
+    def self.known_vulunrabilties(data)
+
     end
 end
 
