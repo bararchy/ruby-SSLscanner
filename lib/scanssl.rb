12ab5ad158317a613a59574029f7683a60bf947a
wolfedale <pawel.grzesik@gmail.com>
2015-11-03 13:48:55 +0000

    Deleted Class Help. Not needed.

--- lib/scanssl.rb
+++ lib/scanssl.rb
@@ -8,19 +8,4 @@ require 'prawn'
 require 'scanssl/version'
 
 module ScanSSL
-  class Help
-    def self.show
-      puts "Usage:"
-      puts "  scanssl [options]"
-      puts ""
-      puts "  Options:"
-      puts "    -v          # show version"
-      puts "    -h          # show help"
-      puts "    -s [host]		# set hostname"
-      puts "    -p [port]		# set port"
-      puts ""
-      puts "  Example:"
-      puts "    scanssl -s google.com -p 443"
-    end
-  end
 end
