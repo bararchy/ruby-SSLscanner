c2bd8e877acf818baa55918bac7ece10dae9d69d
wolfedale <pawel.grzesik@gmail.com>
2015-11-03 14:54:18 +0000

    Added few commands

--- lib/scanssl.rb
+++ lib/scanssl.rb
@@ -24,10 +24,23 @@ module ScanSSL
     end
 
     def call
+      # Here we are going to call some methods.
       puts @server
       puts @port
-      
+     
+      # This is just for test :-)
       PROTOCOLS.each { |x| puts x }
+
+      # In this method I guess we can simply have a kind
+      # of queue for all commands. We can call methods,
+      # get the output and send to colorizeOutput :-)
+    end
+
+    def colorizeOutput(output)
+      # Each method inside the scanssl/ will need to return
+      # something (using return), and here we are going to
+      # create a nice output using those data
+      puts "Here we are going to colorize the output."
     end
   end
 end
